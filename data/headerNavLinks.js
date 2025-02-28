541ddd095d606404a9e06ff9578e9479c6aa8f63
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2025-03-01 03:07:05 +0530

    typo_0.0.1

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,10 +1,10 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
-  { href: '/snippets', title: 'Snippets' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
   { href: '/journey', title: 'Journey' },
+  { href: '/contact', title: 'Contacts' },
 ]
 
 export default headerNavLinks
